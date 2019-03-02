b1493d9912cd44cbad535d79c0cc119e0c0b1bc9
1996linama <31455104+1996linama@users.noreply.github.com>
2019-03-01 17:33:17 -0800

    Added a validator for update quantity input.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,39 +1,59 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Product from './Product'
-import CartItem from './CartItem'
+import React from "react";
+import PropTypes from "prop-types";
+import Product from "./Product";
+import CartItem from "./CartItem";
 
-const Cart = ({ products, total, onCheckoutClicked, removeFromCart, updateCart }) => {
-  const hasProducts = products.length > 0
+const Cart = ({
+  products,
+  total,
+  onCheckoutClicked,
+  removeFromCart,
+  updateCart
+}) => {
+  const hasProducts = products.length > 0;
   const nodes = hasProducts ? (
-    products.map(product =>
+    products.map(product => (
       <CartItem
         product={product}
-        onRemoveFromCartClicked={() => removeFromCart(product.id, product.quantity)}
-        onUpdateCart={(event) => (product.inventory + product.quantity >= event.target.value) ? updateCart(product.id, product.quantity, parseInt(event.target.value, 10)) : alert("Requested quantity is unavailable.") }/>
-    )
+        onRemoveFromCartClicked={() =>
+          removeFromCart(product.id, product.quantity)
+        }
+        onUpdateCart={(event) => {
+          !Number.isInteger(event.target.value) &&
+            alert("Please enter a valid quantity.");
+          product.inventory + product.quantity >= event.target.value
+            ? updateCart(
+                product.id,
+                product.quantity,
+                parseInt(event.target.value, 10)
+              )
+            : alert("Requested quantity is unavailable.");
+        }}
+      />
+    ))
   ) : (
-      <em>Please add some products to cart.</em>
-    )
+    <em>Please add some products to cart.</em>
+  );
 
   return (
     <div>
       <h3>Your Cart</h3>
       <div>{nodes}</div>
       <p>Total: &#36;{total}</p>
-      <button id="checkout" onClick={onCheckoutClicked}
-        disabled={hasProducts ? '' : 'disabled'}>
+      <button
+        id="checkout"
+        onClick={onCheckoutClicked}
+        disabled={hasProducts ? "" : "disabled"}
+      >
         Checkout
       </button>
     </div>
-  )
-}
-//checkInventory(product.id, product.quantity, product.inventory) ? updateCart(product.id, event.target.value) : alert('cannot'
-//(event) => updateCart(product.id, event.target.value)
+  );
+};
 Cart.propTypes = {
   products: PropTypes.array,
   total: PropTypes.string,
   onCheckoutClicked: PropTypes.func
-}
+};
 
-export default Cart
+export default Cart;
